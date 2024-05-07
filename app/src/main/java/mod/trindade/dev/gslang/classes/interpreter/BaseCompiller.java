1789af4dfff358b138d4064cc1ae3f070353e86d
aquilesTrindade <sempedra075@gmail.com>
2024-05-07 08:16:18 -0300

    add gslang for future...

---
+++ app/src/main/java/mod/trindade/dev/gslang/classes/interpreter/BaseCompiller.java
@@ -0,0 +1,56 @@
+package mod.trindade.dev.gslang.classes.interpreter;
+
+//Android
+import android.content.Context;
+import android.widget.Toast;
+
+import mod.trindade.dev.gslang.methods.MethodCaller;
+
+import com.besome.sketch.MainActivity;
+
+public class BaseCompiller {
+	
+	String[] parts;
+	MethodCaller methodCaller;
+	Context mCtx;
+	
+	
+	public BaseCompiller(Context ctx){
+		this.mCtx = ctx;
+        methodCaller = new MethodCaller(mCtx, new MainActivity());
+	}
+	
+	public void compile(String codeToRun){
+		String codeText = codeToRun;
+		parts = codeText.split(" ");
+		if (methodTyped("createButton")) {
+			methodCaller.callMethod(parts[0], parts[1], parts[2]);
+			} else if (methodTyped("createText")) {
+			methodCaller.callMethod(parts[0], parts[1], parts[2]);
+			} else if (methodTyped("showToast")) {
+			methodCaller.callMethod(parts[0], parts[1]);
+			} else if (methodTyped("openTerminal")) {
+			methodCaller.callMethod(parts[0]);
+			} else if (methodTyped("clear"))  {
+			methodCaller.callMethod(parts[0]);
+			} else if (methodTyped("showDialog")) {
+			methodCaller.callMethod(parts[0], parts[1], parts[2]);
+			} else {
+			Toast.makeText(mCtx, "Nenhum método encontrado", 4000).show();
+		}
+	}
+
+
+como eu faço esse codigo suportar  quebras de linha 
+	
+	public boolean methodTyped(String methodName){
+		boolean returnVal;
+		if (parts[0].contains(methodName)) {
+			returnVal = true;
+			} else {
+			returnVal = false;
+		}
+		return  returnVal;
+	}
+	
+}
---
+++ app/src/main/java/mod/trindade/dev/gslang/methods/Methods.java
@@ -0,0 +1,90 @@
+package mod.trindade.dev.gslang.methods;
+
+//Android
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView; 
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+//AndroidX
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.trindade.gamide.R;
+import mod.trindade.dev.theme.ThemedActivity; 
+
+//Java
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import java.lang.ref.WeakReference;
+
+//Google
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class Methods {
+    private Context mCtx;
+    private WeakReference<ThemedActivity> weak;
+    private LinearLayout terminal;
+    private BottomSheetDialog terminalSheet;
+	private View bottomSheetView;
+    
+    public Methods(Context context, ThemedActivity mainActivity) {
+        this.mCtx = context;
+        this.weak = new WeakReference<>(mainActivity);
+		clear();
+        terminalSheet.setCancelable(true);
+        terminal = bottomSheetView.findViewById(R.id.terminal);
+    }
+	
+	public void clear(){
+		bottomSheetView = LayoutInflater.from(mCtx).inflate(R.layout.terminal, null);
+		terminalSheet = new BottomSheetDialog(mCtx);
+		terminalSheet.setContentView(bottomSheetView);
+	}
+    
+    public void openTerminal(){
+        terminalSheet.show();
+    }
+
+    public void createButton(String text, String bgColor) {
+        LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        Button button = new Button(mCtx);
+        button.setLayoutParams(buttonParams);
+        button.setText(text);
+       // button.setBackgroundColor(Color.parseColor(bgColor));
+        terminal.addView(button);
+        openTerminal();
+    }
+
+    public void showToast(String s) {
+        Toast.makeText(mCtx, s, Toast.LENGTH_SHORT).show();
+    }
+
+    public void createText(String textVal, String txtColor) {
+        LinearLayout.LayoutParams txtParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView text = new TextView(mCtx);
+        text.setLayoutParams(txtParams);
+        text.setText(textVal);
+        text.setTextColor(Color.parseColor(txtColor));
+        terminal.addView(text);
+        openTerminal();
+    }
+	
+	public void showDialog (String title, String message){
+		MaterialAlertDialogBuilder d = new MaterialAlertDialogBuilder(mCtx);
+		d.setTitle(title);
+	    d.setMessage(message);
+		d.setPositiveButton("OK", (dd, w) ->{
+					
+		});
+		d.show();
+	}
+}
