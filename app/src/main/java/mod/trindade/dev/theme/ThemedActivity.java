105c87e894912d54dd0e1f19b851e805f34ce2ef
aquilesTrindade <sempedra075@gmail.com>
2024-05-11 22:35:59 -0300

    ThemedActivity improve (remove NavigationBar)

--- app/src/main/java/mod/trindade/dev/theme/ThemedActivity.java
+++ app/src/main/java/mod/trindade/dev/theme/ThemedActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.activity.EdgeToEdge;
 
 import android.os.Bundle;
+import android.view.View;
 
 import mod.trindade.dev.theme.AppTheme;
 
@@ -13,9 +14,18 @@ public class ThemedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle saved) {
-       super.onCreate(saved);
-       appThemeHelper = new AppTheme(this);
-       EdgeToEdge.enable(this);
-       setTheme(appThemeHelper.getTheme());
+        super.onCreate(saved);
+        setTheme();
+        phoneBars();
     }
+    
+    public void setTheme(){
+        appThemeHelper = new AppTheme(this);      
+        setTheme(appThemeHelper.getTheme());
+    }
+    
+    public void phoneBars(){
+        EdgeToEdge.enable(this);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }    
 }
